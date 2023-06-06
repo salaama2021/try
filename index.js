45a83427613b4a5893cac425c2aebafe2f93b168
salaama2021 <134949692+salaama2021@users.noreply.github.com>
2023-06-06 16:57:43 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+
+
+
+//create td
+functiontd_fun(){
+    let td=document.createElement('tr');
+    td.innerHTML =
+    <td class="px-6 py4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                            <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                Thomas
+                            </span>
+                            </div>
+                    </td>
+                            <td class="px-6 py4 whitespace-nowrap">
+                                <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                    Cook
+                                </span> 
+                            </div>
+                        </td>
+
+                                <td class="px-6 py4 whitespace-nowrap">
+                                    <div class="flex items-center">
+                                        <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                            tCo0ks@gmail.com
+                                        </span>
+                                        </div>
+                                </td>
+
+                                        <td class="px-6 py4 whitespace-nowrap">
+                                            <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                                Been part of the team for quite a long time
+                                            </span>
+                                        </div>  
+                                            </td>
+
+                                         <td class="px-6 py4 whitespace-nowrap">
+                                            <div class="flex items-center">
+                                                <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                                    Thomas
+                                                </span>
+                                                </div>
+                                        </td>
+
+                                                <td class="px-6 py4 whitespace-nowrap">
+                                                    <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                                        Cook
+                                                    </span>
+                                                </div>
+                                                </td>
+                                                    
+                                                 <td class="px-6 py4 whitespace-nowrap">
+                                                    <div class="flex items-center">
+                                                        <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                                            Thomas
+                                                        </span>
+                                                        </div>
+                                                </td>
+
+                                                        <td class="px-6 py4 whitespace-nowrap">
+                                                            <span class="px-2 inline-flex-xs leading-5 font-semibold ">
+                                                                Cook
+                                                            </span>
+                                                        </div>
+                                                        </td>
+
+
+                         
+
+                                
+                
